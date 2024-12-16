8922a585bf43dd8275a45b570dcefccde2a6abf5
Akira Tsukiashi <atsukiashi@japanet.co.jp>
2024-12-16 11:00:13 +0900

    Delete postingapp directory

--- postingapp/src/main/java/com/example/postingapp/repository/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.postingapp.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.postingapp.entity.VerificationToken;
-public interface VerificationTokenRepository extends JpaRepository< VerificationToken, Integer> {
-	
-	//トークンに一致するデータをverification_tokensテーブルから探す
-    public VerificationToken findByToken(String token);
-}
